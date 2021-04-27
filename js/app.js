17de5f301f3a28c68aed5d95afeb4257adbaf020
sssshevchenko <denshev233@gmail.com>
2021-04-28 01:37:39 +0300

    waxom

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-	// $('body').hide()
-})
+
+$(document).ready(function () {
+	const swiper = new Swiper('.main-slider', {
+		// Optional parameters
+		loop: true,
+		autoplay: {
+			delay: 3000,
+		},
+		// If we need pagination
+		pagination: {
+		  el: '.swiper-pagination',
+		},
+	  
+		// Navigation arrows
+		navigation: {
+		  nextEl: '.slider__next',
+		  prevEl: '.slider__prev',
+		},
+
+	  });
+
+});
